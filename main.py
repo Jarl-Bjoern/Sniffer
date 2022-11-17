9fe7359f413dc2277b575df166473e45cb4e9534
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-11-17 18:41:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,45 @@ Array_Sniff = []
 Dict_NIC = {}
 
 # Functions
+def Initialien():
+    print ("----------------------------------------")
+    print ("|                Sniffer                |")
+    print ("|              Version 0.1              |")
+    print ("|             Rainer Herold             |")
+    print ("----------------------------------------\n")
+
 def Sniff_Packets(pkt):
+        #def Filter(x):
+        #    if (':' in str(x)):
+        #        if (str(x).split(':')[0] in Dict_NIC): return f'Interface: {Dict_NIC[IP[5]]} -> '
+        #    else:
+        #        if (str(IP[5]) in Dict_NIC): return f'Interface: {Dict_NIC[IP[5]]} -> '
+
         Filter = str(pkt.summary())
         IP = Filter.split(' ')
         Result = ''
-        if (str(IP[5]) in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[5]]} -> '
-        elif (str(IP[7]) in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[7]]} -> '
-        Result += f'Source: {IP[5]} -> Destination: {IP[7]}'
-        if (Result not in Array_Sniff):
-                Array_Sniff.append(Result)
-                if (not exists("/opt/Tools")): makedirs("/opt/Tools")
-                with open('/opt/Tools/sniffer-result.txt', 'a') as f: f.write(f'{Result}\n')
+        if ('.' in str(IP[5]) and '.' in str(IP[7])):
+            if (':' in str(IP[5])):
+                if (str(IP[5]).split(':')[0] in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[5]]} -> '
+            else:
+                if (str(IP[5]) in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[5]]} -> '
+
+            if (':' in str(IP[7])):
+                if (str(IP[7]).split(':')[0] in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[7]]} -> '
+            else:
+                if (str(IP[7]) in Dict_NIC): Result = f'Interface: {Dict_NIC[IP[7]]} -> '
+
+            if (exists("/opt/Tools/sniffer-result.txt")):
+                with open('/opt/Tools/sniffer-result.txt', 'r') as f:
+                    Array_Sniff = f.read().splitlines()
+
+            Result += f'Source: {IP[5]} -> Destination: {IP[7]}'
+            if (Result not in Array_Sniff):
+                    Array_Sniff.append(Result)
+                    if (not exists("/opt/Tools")): makedirs("/opt/Tools")
+                    with open('/opt/Tools/sniffer-result.txt', 'a') as f: f.write(f'{Result}\n')
+                    print (Result)
+        else: print(Filter)
 
 def main():
         NICS = getoutput ('ifconfig | grep -v "LOOPBACK" | grep "flags" | cut -d " " -f1').splitlines()
@@ -49,4 +77,4 @@ def main():
 
 # Main
 if __name__ == '__main__':
-        main()
+        Initialien(), main()
